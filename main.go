9aadd66481ab4d6a20d094cfd48ca7d55014d5d0
Ruslan Gustomiasov <rgustomiasov@mirantis.com>
2019-09-18 15:31:47 +0200

    commit

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/atlassian/go-artifactory/v2/artifactory"
+	"github.com/atlassian/go-artifactory/v2/artifactory/transport"
+	"github.com/atlassian/go-artifactory/v2/artifactory/v1"
+	"os"
+)
+
+func main() {
+	tp := transport.BasicAuth{
+		Username: os.Getenv("ARTIFACTORY_USERNAME"),
+		Password: os.Getenv("ARTIFACTORY_PASSWORD"),
+	}
+
+	client, err := artifactory.NewClient(os.Getenv("ARTIFACTORY_URL"), tp.Client())
+	if err != nil {
+		fmt.Printf("\nerror: %v\n", err)
+		return
+	}
+	opts := v1.RepositoryListOptions{
+		Type: "local",
+	}
+	repos, _, err := client.V1.Repositories.ListRepositories(context.Background(), &opts)
+	if err != nil {
+		fmt.Printf("\nerror: %v\n", err)
+		return
+	} else if repos == nil {
+		fmt.Printf("\nerror: repos cannot be nil\n")
+		return
+	}
+
+	fmt.Println("Found these local repos:")
+	for _, repo := range *repos {
+		fmt.Println(repo.Key)
+	}
+}

8dd3d9d7170b3a458113825d79bfb9c8217cd1a4
Ruslan Gustomiasov <rgustomiasov@mirantis.com>
2019-09-20 16:22:29 +0200

    blah

--- main.go
+++ main.go
@@ -1,16 +1,36 @@
 package main
 
 import (
+	"context"
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
 	"io/ioutil"
 	"net/http"
 	"os"
 	"strings"
 )
 
-func get_token(docker_registry string) string {
-	resp, err := http.Get(docker_registry + "/artifactory/api/docker/docker-prod-local/v2/token")
+type Creds struct {
+	SourceUser          string
+	SourcePassword      string
+	DestinationUser     string
+	DestinationPassword string
+}
+
+type ImageToReplicate struct {
+	SourceRegistry      string
+	SourceImage         string
+	DestinationRegistry string
+	DestinationImage    string
+	SourceTag           string
+	DestinationTag      string
+}
+
+func GetToken(docker_registry string) string {
+	resp, err := http.Get("https://" + docker_registry + "/artifactory/api/docker/docker-prod-local/v2/token")
 	if err != nil {
 		panic(err)
 	}
@@ -31,9 +51,9 @@ func get_token(docker_registry string) string {
 	return (b.Token)
 }
 
-func get_repos(docker_registry string, token string) []string {
+func GetRepos(docker_registry string, token string) []string {
 	client := &http.Client{}
-	req, err := http.NewRequest("GET", docker_registry+"/v2/_catalog", nil)
+	req, err := http.NewRequest("GET", "https://"+docker_registry+"/v2/_catalog", nil)
 	if err != nil {
 		panic(err)
 	}
@@ -55,25 +75,146 @@ func get_repos(docker_registry string, token string) []string {
 	return b.Repositories
 }
 
+func listTags(docker_registry string, image string, token string) []string {
+	httpClient := &http.Client{}
+	req, err := http.NewRequest("GET", "https://"+docker_registry+"/v2/"+image+"/tags/list", nil)
+	if err != nil {
+		panic(err)
+	}
+	req.Header.Add("Authorization", "Bearer "+token)
+	resp, err := httpClient.Do(req)
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+	type res struct {
+		Name string
+		Tags []string
+	}
+	var b res
+	err = json.Unmarshal(body, &b)
+	if err != nil {
+		panic(err)
+	}
+	return b.Tags
+}
+
+func pullImage(image ImageToReplicate, creds Creds) error {
+	ctx := context.Background()
+	cli, err := client.NewClientWithOpts(client.FromEnv)
+	if err != nil {
+		return err
+	}
+	cli.NegotiateAPIVersion(ctx)
+	if creds.SourceUser != "" && creds.SourcePassword != "" {
+		authConfig := types.AuthConfig{
+			Username: creds.SourceUser,
+			Password: creds.SourcePassword,
+		}
+		encodedJSON, err := json.Marshal(authConfig)
+		if err != nil {
+			return err
+		}
+		authStr := base64.URLEncoding.EncodeToString(encodedJSON)
+		_, err = cli.ImagePull(ctx, image.SourceRegistry+"/"+image.SourceImage+":"+image.SourceTag, types.ImagePullOptions{RegistryAuth: authStr})
+		if err != nil {
+			return err
+		}
+	} else {
+		_, err := cli.ImagePull(ctx, image.SourceRegistry+"/"+image.SourceImage+":"+image.SourceTag, types.ImagePullOptions{})
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func pushImage(image ImageToReplicate, creds Creds) error {
+	ctx := context.Background()
+	cli, err := client.NewClientWithOpts(client.FromEnv)
+	if err != nil {
+		return err
+	}
+	cli.NegotiateAPIVersion(ctx)
+	if creds.DestinationUser != "" && creds.DestinationPassword != "" {
+		authConfig := types.AuthConfig{
+			Username: creds.DestinationUser,
+			Password: creds.DestinationPassword,
+		}
+		encodedJSON, err := json.Marshal(authConfig)
+		if err != nil {
+			return err
+		}
+		authStr := base64.URLEncoding.EncodeToString(encodedJSON)
+		_, err = cli.ImagePush(ctx, image.DestinationImage+":"+image.DestinationTag, types.ImagePushOptions{RegistryAuth: authStr})
+		if err != nil {
+			return err
+		}
+	} else {
+		_, err := cli.ImagePush(ctx, image.DestinationImage+":"+image.DestinationTag, types.ImagePushOptions{})
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func replicate(image ImageToReplicate, creds Creds) error {
+	fmt.Printf("%s/%s:%s -> %s/%s:%s\n", image.SourceRegistry, image.SourceImage, image.SourceTag, image.DestinationRegistry, image.DestinationImage, image.DestinationTag)
+	err := pullImage(image, creds)
+	if err != nil {
+		return err
+	}
+	err = pushImage(image, creds)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
 func main() {
-	dockerRegistry := os.Getenv("DOCKER_REGISTRY")
-	if dockerRegistry == "" {
-		panic("empty DOCKER_REGISTRY env variable")
+	sourceRegistry := os.Getenv("SOURCE_REGISTRY")
+	if sourceRegistry == "" {
+		panic("empty SOURCE_REGISTRY env variable")
+	}
+	destinationRegistry := os.Getenv("DESTINATION_REGISTRY")
+	if destinationRegistry == "" {
+		panic("empty DESTINATION_REGISTRY env variable")
+	}
+	creds := Creds{
+		SourceUser:          os.Getenv("SOURCE_USER"),
+		SourcePassword:      os.Getenv("SOURCE_PASSWORD"),
+		DestinationUser:     os.Getenv("DESTINATION_USER"),
+		DestinationPassword: os.Getenv("DESTINATION_PASSWORD"),
 	}
 	imageFilter := os.Getenv("IMAGE_FILTER")
-	token := get_token(dockerRegistry)
-	repos := get_repos(dockerRegistry, token)
+	token := GetToken(sourceRegistry)
+	repos := GetRepos(sourceRegistry, token)
 	resultRepos := repos[:0]
 	if imageFilter != "" {
 		for _, repo := range repos {
-			if strings.HasPrefix(repo, imageFilter){
+			if strings.HasPrefix(repo, imageFilter) {
 				resultRepos = append(resultRepos, repo)
 			}
 		}
 	} else {
 		resultRepos = repos
 	}
-	for _, repo :=  range resultRepos{
-		fmt.Println(repo)
+	for _, repo := range resultRepos {
+		for _, tag := range listTags(sourceRegistry, repo, token) {
+			image := ImageToReplicate{
+				SourceRegistry:      sourceRegistry,
+				SourceImage:         repo,
+				DestinationRegistry: destinationRegistry,
+				DestinationImage:    repo,
+				SourceTag:           tag,
+				DestinationTag:      tag,
+			}
+			err := replicate(image, creds)
+			if err != nil {
+				panic(err)
+			}
+		}
 	}
 }

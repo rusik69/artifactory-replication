33f552504064bbe5d92d7e243a4c08f4947ed5cc
Ruslan Gustomiasov <rgustomiasov@mirantis.com>
2019-09-20 13:59:44 +0200

    commit

--- main.go
+++ main.go
@@ -1,39 +1,79 @@
 package main
 
 import (
-	"context"
+	"encoding/json"
 	"fmt"
-	"github.com/atlassian/go-artifactory/v2/artifactory"
-	"github.com/atlassian/go-artifactory/v2/artifactory/transport"
-	"github.com/atlassian/go-artifactory/v2/artifactory/v1"
+	"io/ioutil"
+	"net/http"
 	"os"
+	"strings"
 )
 
+func get_token(docker_registry string) string {
+	resp, err := http.Get(docker_registry + "/artifactory/api/docker/docker-prod-local/v2/token")
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+	type res struct {
+		Token string
+		TTL   uint64
+	}
+	var b res
+	err = json.Unmarshal(body, &b)
+	if err != nil {
+		panic(err)
+	}
+	return (b.Token)
+}
+
+func get_repos(docker_registry string, token string) []string {
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", docker_registry+"/v2/_catalog", nil)
+	if err != nil {
+		panic(err)
+	}
+	req.Header.Add("Authorization", "Bearer "+token)
+	resp, err := client.Do(req)
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+	type res struct {
+		Repositories []string
+	}
+	var b res
+	err = json.Unmarshal(body, &b)
+	if err != nil {
+		panic(err)
+	}
+	return b.Repositories
+}
+
 func main() {
-	tp := transport.BasicAuth{
-		Username: os.Getenv("ARTIFACTORY_USERNAME"),
-		Password: os.Getenv("ARTIFACTORY_PASSWORD"),
+	dockerRegistry := os.Getenv("DOCKER_REGISTRY")
+	if dockerRegistry == "" {
+		panic("empty DOCKER_REGISTRY env variable")
 	}
-
-	client, err := artifactory.NewClient(os.Getenv("ARTIFACTORY_URL"), tp.Client())
-	if err != nil {
-		fmt.Printf("\nerror: %v\n", err)
-		return
+	imageFilter := os.Getenv("IMAGE_FILTER")
+	token := get_token(dockerRegistry)
+	repos := get_repos(dockerRegistry, token)
+	resultRepos := repos[:0]
+	if imageFilter != "" {
+		for _, repo := range repos {
+			if strings.HasPrefix(repo, imageFilter){
+				resultRepos = append(resultRepos, repo)
+			}
+		}
+	} else {
+		resultRepos = repos
 	}
-	opts := v1.RepositoryListOptions{
-		Type: "local",
-	}
-	repos, _, err := client.V1.Repositories.ListRepositories(context.Background(), &opts)
-	if err != nil {
-		fmt.Printf("\nerror: %v\n", err)
-		return
-	} else if repos == nil {
-		fmt.Printf("\nerror: repos cannot be nil\n")
-		return
-	}
-
-	fmt.Println("Found these local repos:")
-	for _, repo := range *repos {
-		fmt.Println(repo.Key)
+	for _, repo :=  range resultRepos{
+		fmt.Println(repo)
 	}
 }
